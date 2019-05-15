67d03ef7f6b0378aba2fbc6718828a539999597c
ynjacobs <ytj64611@gamil.com>
2019-05-15 00:54:44 -0400

    started adding instance and class methods

---
+++ part4.py
@@ -0,0 +1,23 @@
+class Vampire:
+    coven = []
+    def __init__(self, name, age, in_coffin, drank_blood_today):
+        self.name = name
+        self.age = age
+        self.in_coffin = bool
+        self.drank_blood_today = bool
+
+    @classmethod
+    def create(cls, ):
+        new_vampire = Vampire(name, age, in_coffin, drank_blood_today)
+        cls.coven.append(new_vampire)
+        return new_vampire
+
+    def drink_blood():
+        return self.drink_blood_today = True
+
+    def sunrise():
+        if self.in_coffin = False or self.drank_blood_today = False:
+            Vampire.coven.append()
+
+
+    def go_home():
