136922a74d07f5150e8f0a9aa9d0d8714e55005a
ynjacobs <ytj64611@gamil.com>
2019-05-15 12:40:59 -0400

    finised more methods and fixed an error in the code

--- part4.py
+++ part4.py
@@ -1,23 +1,51 @@
 class Vampire:
     coven = []
-    def __init__(self, name, age, in_coffin, drank_blood_today):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.in_coffin = bool
-        self.drank_blood_today = bool
+        self.in_coffin = True
+        self.drank_blood_today = True
 
     @classmethod
-    def create(cls, ):
-        new_vampire = Vampire(name, age, in_coffin, drank_blood_today)
+    def create(cls, name, age):
+        new_vampire = Vampire(name, age)
         cls.coven.append(new_vampire)
         return new_vampire
 
-    def drink_blood():
-        return self.drink_blood_today = True
+    def drink_blood(self):
+        self.drank_blood_today = True
+         
+    @classmethod
+    def sunrise(cls):
+        for self in Vampire.coven:
+            if self.in_coffin == False or self.drank_blood_today == False:
+             Vampire.coven.remove(self)
 
-    def sunrise():
-        if self.in_coffin = False or self.drank_blood_today = False:
-            Vampire.coven.append()
+    @classmethod
+    def sunset(cls):
+        for self in Vampire.coven:
+         self.drank_blood_today = False
+         self.in_coffin = False
 
 
-    def go_home():
+    def go_home(self):
+        self.in_coffin = True
+
+    def __repr__(self):
+        return ' {} , {}-{} '.format(self.name, self.drank_blood_today, self.in_coffin)
+
+the_vampire = Vampire.create('John', '45')
+vampire2 = Vampire.create('Max', '66')
+Vampire.sunset()
+print(the_vampire)
+the_vampire.drink_blood()
+the_vampire.go_home()
+print(the_vampire)
+
+vampire2.drink_blood()
+
+
+print(vampire2)
+
+Vampire.sunrise()
+print(Vampire.coven)
